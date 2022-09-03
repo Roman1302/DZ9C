7e9b40ee607d26b3e743278475c6e8cc63253963
Роман Бухмистов <rrb13@mail.ru>
2022-09-03 23:40:01 +0300

    корректировка недочетов

--- task68/Program.cs
+++ task68/Program.cs
@@ -2,25 +2,25 @@
 Даны два неотрицательных числа m и n.
 m = 2, n = 3 -> A(m,n) = 29 */
 
-int A(int n, int m)
+int Ackerman(int n, int m)
 {
     if (n == 0)
         return m + 1;
     else
       if ((n != 0) && (m == 0))
-        return A(n - 1, 1);
+        return Ackerman(n - 1, 1);
     else
-        return A(n - 1, A(n, m - 1));
+        return Ackerman(n - 1, Ackerman(n, m - 1));
 }
 
 int Input(string messege)
 {
-InputValue:
+InputVackermanlue:
     Console.Write(messege);
     if (!int.TryParse(Console.ReadLine()!, out int value))
     {
         Console.WriteLine("Что-то пошло не так");
-        goto InputValue;
+        goto InputVackermanlue;
     }
     return value;
 }
@@ -29,4 +29,4 @@ int m = Input("Введите начальное число (M): ");
 int n = Input("Введите конечное число (N): ");
 
 Console.WriteLine();
-Console.WriteLine($"m = {m}, n = {n} -> A(m,n) = {A(n, m)}");
+Console.WriteLine($"m = {m}, n = {n} -> A(m,n) = {Ackerman(n, m)}");
